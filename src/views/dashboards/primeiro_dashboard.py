22c0d987d6992bc7ccf5f18f1e1b9e91fcf6aa55
goncaze <goncaze@gmail.com>
2025-06-03 14:40:58 -0300

    Otimização do código primeiro_dashboard.py

--- src/views/dashboards/primeiro_dashboard.py
+++ src/views/dashboards/primeiro_dashboard.py
@@ -14,8 +14,6 @@ class PrimeiroDashboard(ft.View):
     ):
         super().__init__()
 
-        # self.expand = True
-        # self.scroll = True
         self.page = page
         self.dbs = dbs
         self.data_db = DataDB(self.dbs)
@@ -24,34 +22,19 @@ class PrimeiroDashboard(ft.View):
         self.dias_letivos_card.margin = ft.margin.only(top=10)
 
         self.route = "/"
-        # self.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
         self.vertical_alignment = ft.MainAxisAlignment.START
         self.cor_fundo = ft.Colors.WHITE  # "#afffbf"
         self.bgcolor = self.cor_fundo  # ft.Colors.WHITE
         self.padding = 0
         self.spacing = 0
-        # self.appbar = ft.AppBar(title=ft.Text("CALINT"))
         self.navegador = Navegador(self.page)
         self.drawer_pagelet = self.navegador.drawer()
 
         self.dias_mes_corrente = self.data_db.select_dias_mes_corrente()
         self.todas_dt_literal = self.extrair_dt_literal()
 
-        # print(f"self.hoje_card: {self.hoje_card}")
-        # print(f"self.hoje_card.ano: {self.hoje_card.ano}")
-        # print(f"self.hoje_card.mes: {self.hoje_card.mes}")
-
-        self.my_safe_area = ft.Container(
-            height=40,
-            # bgcolor=self.cor_fundo,
-            bgcolor=ft.Colors.GREEN_900,
-            margin=ft.margin.all(0),
-            padding=ft.padding.all(0),
-        )
         self.my_base_area = ft.Container(
             height=50,
-            # bgcolor=self.cor_fundo,
-            # bgcolor=ft.Colors.GREEN_900,
         )
 
         self.mes_card = MesCard(
@@ -65,25 +48,11 @@ class PrimeiroDashboard(ft.View):
 
         self.mes_card.margin = ft.margin.only(left=10, right=10, top=10)
 
-        self.lv_pagelet = ft.ListView(
-            spacing=0,
-            padding=0,
-            expand=True,
-            # horizontal=True,
-        )
-        ##################################################################################
-        #
-        # self.lv_pagelet.controls.append(
-        #     ft.Container(
-        #         height=50,
-        #         # bgcolor=self.cor_fundo,
-        #         bgcolor=ft.Colors.GREEN_900,
-        #     )
-        # )
+        self.sublayout = ft.Column(expand=True, scroll=ft.ScrollMode.ALWAYS)
 
-        self.lv_pagelet.controls.append(self.hoje_card)
-        self.lv_pagelet.controls.append(self.mes_card)
-        self.lv_pagelet.controls.append(
+        self.sublayout.controls.append(self.hoje_card)
+        self.sublayout.controls.append(self.mes_card)
+        self.sublayout.controls.append(
             ft.Container(
                 content=self.mes_card.coluna_de_eventos,
                 bgcolor="#F7F7F7",
@@ -93,12 +62,13 @@ class PrimeiroDashboard(ft.View):
                 padding=ft.padding.only(left=7, top=10, bottom=2),
             )
         )
-        self.lv_pagelet.controls.append(self.dias_letivos_card)
-        self.lv_pagelet.controls.append(self.my_base_area)
+
+        self.sublayout.controls.append(self.dias_letivos_card)
+        self.sublayout.controls.append(self.my_base_area)
 
         self.pagelet = ft.Pagelet(
             expand=True,
-            content=self.lv_pagelet,
+            content=ft.SafeArea(self.sublayout),
             bgcolor=self.cor_fundo,
             bottom_app_bar=ft.BottomAppBar(
                 height=65,
@@ -106,7 +76,6 @@ class PrimeiroDashboard(ft.View):
                 shape=ft.NotchShape.CIRCULAR,
                 content=ft.Container(
                     alignment=ft.alignment.bottom_center,
-                    # bgcolor=ft.Colors.PINK,
                     content=ft.Text(
                         value="Calendário Acadêmico do IFMA Campus Viana",
                         theme_style=ft.TextThemeStyle.BODY_MEDIUM,
@@ -116,24 +85,16 @@ class PrimeiroDashboard(ft.View):
             ),
             end_drawer=self.drawer_pagelet,
             floating_action_button=ft.FloatingActionButton(
-                # text="Menu",
-                # content=ft.Text(value="Menu", color=ft.Colors.WHITE),
                 content=ft.Icon(name=ft.Icons.SETTINGS, color=ft.Colors.WHITE, size=40),
                 on_click=self.open_pagelet_end_drawer,
                 shape=ft.CircleBorder(),
                 bgcolor=ft.Colors.RED,
-                # icon=ft.Icons.AC_UNIT,
             ),
             floating_action_button_location=ft.FloatingActionButtonLocation.CENTER_DOCKED,
         )
         #
         # ################################################################################
-        self.controls = [
-            # ft.Image(src="Campus_Viana_edited.png", color="green"),
-            # self.hoje_card,
-            self.my_safe_area,
-            self.pagelet,
-        ]
+        self.controls = [self.pagelet]
 
     def open_pagelet_end_drawer(self, e):
         self.pagelet.show_drawer(self.drawer_pagelet)
